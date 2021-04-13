7e28c1d164a2098baf45c59445818c16c0df41e2
MinersWin <minerswin@minerswin.de>
2021-04-13 08:33:38 +0200

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -20,15 +20,15 @@ require './phpmailer/src/SMTP.php';
   // Receiver's Email
 --------------------------------------------- */
 
-$toEmail = "your-email@website.com"; // Replace Your Email Address
+$toEmail = "kontakt@moritz-mantel.de"; // Replace Your Email Address
 
 
 /* --------------------------------------------
   // Sender's Email
 --------------------------------------------- */
 
-$fromEmail = "no-reply@website.com";  // Replace Company's Email Address (preferably currently used Domain Name)
-$fromName = "Company Name"; // Replace Company Name
+$fromEmail = "kontaktformular@moritz-mantel.de";  // Replace Company's Email Address (preferably currently used Domain Name)
+$fromName = "Moritz-Mantel.de"; // Replace Company Name
 
 
 /* --------------------------------------------
@@ -42,7 +42,7 @@ $recaptcha_secret = 'YOUR_RECAPTCHA_SECRET_KEY'; // Your Google reCaptcha Secret
 /* --------------------------------------------
   // Subject
 --------------------------------------------- */
-$subject = "Form Response from Your Website"; // Your Subject
+$subject = "Kontaktformular auf Moritz-Mantel.de"; // Your Subject
 
 
 if (isset($_POST['name'])) {
